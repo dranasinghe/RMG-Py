2332131da9b5e4a35b5db2310afe27f0e7c69387
Agnes <ajocher@mit.edu>
2018-12-17 13:29:46 -0500

    Split reaction families in parallel reaction generation for improved load balancing.

--- rmgpy/rmg/react.py
+++ rmgpy/rmg/react.py
@@ -81,16 +81,17 @@ def react(*spcTuples):
     return itertools.chain.from_iterable(reactions)
 
 
-def reactSpecies(speciesTuple):
+def reactSpecies(speciesTupleTmp):
     """
     Given a tuple of Species objects, generates all possible reactions
     from the loaded reaction families and combines degenerate reactions.
 
-    The generated reactions are deflated.
-    """
+    speciesTuple = speciesTupleTmp[0:-1]
+    own_families = speciesTupleTmp[-1]
+
     speciesTuple = tuple([spc.copy(deep=True) for spc in speciesTuple])
 
-    reactions = getDB('kinetics').generate_reactions_from_families(speciesTuple)
+    reactions = getDB('kinetics').generate_reactions_from_families(speciesTuple, only_families=own_families)
 
     return reactions
 
@@ -122,5 +123,119 @@ def reactAll(coreSpcList, numOldCoreSpecies, unimolecularReact, bimolecularReact
                         if coreSpcList[i].reactive and coreSpcList[j].reactive and coreSpcList[k].reactive:
                             spcTuples.append((coreSpcList[i], coreSpcList[j], coreSpcList[k]))
 
-    rxns = list(react(*spcTuples))
+    # Load kineticsFamilies to be added to reactant tuple to allow for improved load balancing 
+    # in parallel jobs
+    splitListOrig = []
+    splitListTmp = []
+    for key, value in getDB('kinetics').families.iteritems() :
+        splitListOrig.append(key)
+        splitListTmp.append(key)
+ 
+    # Identify and split families that are prone to generate many reactions into sublists
+    splitList = []
+    for (i, v) in enumerate (splitListTmp):
+        if splitListTmp[i] == 'H_Abstraction':
+            splitListTmp[i] = []
+            splitList.append(['H_Abstraction'])
+        elif splitListTmp[i] == 'R_Recombination':
+            splitListTmp[i] = []
+            splitList.append(['R_Recombination'])
+        elif splitListTmp[i] == 'Intra_Disproportionation':
+            splitListTmp[i] = []
+            splitList.append(['Intra_Disproportionation'])
+        elif splitListTmp[i] == 'Intra_RH_Add_Endocyclic':
+            splitListTmp[i] = []
+            splitList.append(['Intra_RH_Add_Endocyclic'])
+        elif splitListTmp[i] == 'Singlet_Carbene_Intra_Disproportionation':
+            splitListTmp[i] = []
+            splitList.append(['Singlet_Carbene_Intra_Disproportionation'])
+        elif splitListTmp[i] == 'Intra_ene_reaction':
+            splitListTmp[i] = []
+            splitList.append(['Intra_ene_reaction'])
+        elif splitListTmp[i] == 'Disproportionation':
+            splitListTmp[i] = []
+            splitList.append(['Disproportionation'])
+        elif splitListTmp[i] == '1,4_Linear_birad_scission':
+            splitListTmp[i] = []
+            splitList.append(['1,4_Linear_birad_scission'])
+        elif splitListTmp[i] == 'R_Addition_MultipleBond':
+            splitListTmp[i] = []
+            splitList.append(['R_Addition_MultipleBond'])
+        elif splitListTmp[i] == '2+2_cycloaddition_Cd':
+            splitListTmp[i] = []
+            splitList.append(['2+2_cycloaddition_Cd'])
+        elif splitListTmp[i] == 'Diels_alder_addition':
+            splitListTmp[i] = []
+            splitList.append(['Diels_alder_addition'])
+        elif splitListTmp[i] == 'Intra_RH_Add_Exocyclic':
+            splitListTmp[i] = []
+            splitList.append(['Intra_RH_Add_Exocyclic'])
+        elif splitListTmp[i] == 'Intra_Retro_Diels_alder_bicyclic':
+            splitListTmp[i] = []
+            splitList.append(['Intra_Retro_Diels_alder_bicyclic'])
+        elif splitListTmp[i] == 'Intra_2+2_cycloaddition_Cd':
+            splitListTmp[i] = []
+            splitList.append(['Intra_2+2_cycloaddition_Cd'])
+        elif splitListTmp[i] == 'Birad_recombination':
+            splitListTmp[i] = []
+            splitList.append(['Birad_recombination'])
+        elif splitListTmp[i] == 'Intra_Diels_alder_monocyclic':
+            splitListTmp[i] = []
+            splitList.append(['Intra_Diels_alder_monocyclic'])
+        elif splitListTmp[i] == '1,4_Cyclic_birad_scission':
+            splitListTmp[i] = []
+            splitList.append(['1,4_Cyclic_birad_scission'])
+        elif splitListTmp[i] == '1,2_Insertion_carbene':
+            splitListTmp[i] = []
+            splitList.append(['1,2_Insertion_carbene'])
+
+    splitList.append(splitListTmp)
+
+    # Only employ family splitting for reactants that have a larger number of atoms than nAFS
+    nAFS = 10
+    #print ("splitList {0}").format(splitList)
+    #print ("Length spcTuples {0}").format(len(spcTuples))
+    spcTuplestmp = []
+    # Append reaction families to reactant tuple
+    for tmpj in spcTuples:
+        if len(tmpj) == 1:
+            if len(str(tmpj[0])) > nAFS:
+                for tmpl in splitList: 
+                    tmpk = list(tmpj)
+                    tmpk.append(tmpl)
+                    spcTuplestmp.append(tuple(tmpk))
+            else:
+                tmpk = list(tmpj)
+                tmpk.append(splitListOrig)
+                spcTuplestmp.append(tuple(tmpk))
+        elif len(tmpj) == 2:
+            if (len(str(tmpj[0])) > nAFS
+               ) or (len(str(tmpj[1])) > nAFS):
+                for tmpl in splitList:
+                    tmpk = list(tmpj)
+                    tmpk.append(tmpl)
+                    spcTuplestmp.append(tuple(tmpk))
+            else:
+                tmpk = list(tmpj)
+                tmpk.append(splitListOrig)
+                spcTuplestmp.append(tuple(tmpk))
+        elif len(tmpk) == 3:
+            if (len(str(tmpk[0])) > nAFS
+               ) or (len(str(tmpk[1])) > nAFS
+                    ) or (len(str(tmpk[2])) > nAFS):
+                for tmpl in splitList:
+                    tmpk = list(tmpj)
+                    tmpk.append(tmpl)
+                    spcTuplestmp.append(tuple(tmpk))
+            else:
+                tmpk = list(tmpj)
+                tmpk.append(splitListOrig)
+                spcTuplestmp.append(tuple(tmpk))
+
+#    print spcTuplestmp
+#    print ("Length spcTuplestmp {0}").format(len(spcTuplestmp))
+
+    rxns = list(react(*spcTuplestmp))
+
     return rxns
+
